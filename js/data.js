886533154e7531d1a13944d9c775a743d5955f9f
manu-devops98 <manuelcappello7@gmail.com>
2021-12-13 15:36:31 +0100

    add Milestone 1 and 2

--- js/data.js
+++ js/data.js
@@ -1,14 +1,18 @@
 /* Milestone 1
 Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+
 Milestone 2
 Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+
 Milestone 3
 Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-BONUS
+
+BONUS:
 1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+
 2- popolare le options della select della milestone 3 dinamicamente. */
 
-[
+const icons = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -122,3 +126,15 @@ BONUS
 		color: 'blue'
 	}
 ];
+
+const container = document.querySelector('.container');
+
+icons.forEach((element) => {
+	const templateCard = `
+	 <div class="card ${element.type}">
+		<i class="${element.family} ${element.prefix}${element.name}" style = "color: ${element.color}"></i>
+		<h4>${element.name.toUpperCase()}</h4>
+	</div> 
+	`;
+	container.innerHTML += templateCard;
+});
